94ec823c5d6cc8561e3a9ec71d0aa2a557782aba
tungnt2 <tungnt2@vnpay.vn>
2025-03-27 17:16:45 +0700

    feat: add calendar-range component to registry.json

--- src/components/ui/calendar/el-calendar-weeks.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-import { WeeksProps } from "react-day-picker"
-
-const CalendarWeeks: React.FC<WeeksProps> = ({ ...props }) => {
-  return <div {...props} />
-}
-
-export default CalendarWeeks
