f3be7eb0213f48ef73fb57502ac0d1f26a0b5aa3
tungnt2 <tungnt2@vnpay.vn>
2026-01-15 18:23:23 +0700

    feat: Introduce SVGInline component for rendering SVGs with unique IDs and a loading skeleton.

--- src/components/ui/svg-inline/svg-inline.tsx
+++ src/components/ui/svg-inline/svg-inline.tsx
@@ -1,22 +1,96 @@
-"use client";
+"use client"
 
-import { cn } from "@/lib/utils";
-import { ReactSVG, Props as ReactSVGProps } from "react-svg";
+import { useId } from "react"
 
-import { Skeleton } from "../skeleton";
-import styles from "./svg-inline.module.css";
+import { cn } from "@/lib/utils"
+import { ReactSVG, Props as ReactSVGProps } from "react-svg"
+
+import { Skeleton } from "../skeleton"
+import styles from "./svg-inline.module.css"
 
 // Exclude 'ref' from the props
-type SVGInlineProps = Omit<ReactSVGProps, "ref">;
+type SVGInlineProps = Omit<ReactSVGProps, "ref">
 
-function SVGInline({ className, ...props }: SVGInlineProps) {
+/**
+ * Extract SVG filename without extension from src path
+ */
+function getSvgName(src: string): string {
+  const urlWithoutQuery = src.split("?")[0]
+  const filename = urlWithoutQuery.split("/").pop() || ""
+  return filename.split(".")[0]
+}
+
+/**
+ * Modify all IDs in SVG to be unique and update references
+ */
+function modifySvgIds(svg: SVGElement, uniqueId: string): void {
+  const elementsWithId = svg.querySelectorAll("[id]")
+  const idMap = new Map<string, string>()
+
+  // First pass: collect all IDs and generate unique versions
+  elementsWithId.forEach((element) => {
+    const originalId = element.id
+    const newId = `${originalId}-${uniqueId}`
+    idMap.set(originalId, newId)
+    element.setAttribute("id", newId)
+  })
+
+  if (idMap.size > 0) {
+    const allElements = svg.getElementsByTagName("*")
+    for (const element of Array.from(allElements)) {
+      for (const attr of Array.from(element.attributes)) {
+        let newValue = attr.value
+        idMap.forEach((newId, originalId) => {
+          newValue = newValue
+            .replace(
+              new RegExp(`url\\(#${originalId}\\)`, "g"),
+              `url(#${newId})`
+            )
+            .replace(new RegExp(`#${originalId}(?![\\w-])`, "g"), `#${newId}`)
+        })
+        if (newValue !== attr.value) {
+          attr.value = newValue
+        }
+      }
+    }
+  }
+}
+
+function SVGInline({
+  className,
+  src,
+  beforeInjection,
+  ...props
+}: SVGInlineProps) {
+  const reactId = useId()
+  // Create a stable unique suffix from React's useId
+  const uniqueId = reactId.replace(/:/g, "")
+
+  const svgName = src ? getSvgName(src) : ""
+  const svgClassName = svgName ? `icon-svg-${svgName}` : ""
+
+  const handleBeforeInjection = (svg: SVGSVGElement) => {
+    // Add class based on SVG name
+    if (svgClassName) {
+      svg.classList.add(svgClassName)
+    }
+
+    // Modify IDs to be unique
+    modifySvgIds(svg, uniqueId)
+
+    // Call original beforeInjection if provided
+    beforeInjection?.(svg)
+  }
+
   return (
     <ReactSVG
       {...props}
-      className={cn("relative", styles.svg, className)}
-      loading={() => <Skeleton className={cn("rounded-xl absolute inset-0")} />}
+      src={src}
+      beforeInjection={handleBeforeInjection}
+      className={cn("relative", styles.svg, svgClassName, className)}
+      loading={() => <Skeleton className={cn("absolute inset-0 rounded-xl")} />}
     />
-  );
+  )
 }
 
-export { SVGInline };
+export { SVGInline }
